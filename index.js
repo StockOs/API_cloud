ea6c9cf7737c975c27d62f61b82c72429c65eb0f
Alexanne Brunold <alexanne.brunold@hetic.net>
2021-03-09 14:35:38 +0100

    ADD Config heroku

--- index.js
+++ index.js
@@ -21,28 +21,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 require("./src/routes/routes.js")(app);
 
-const PORT = process.env.API_PORT || 80;
-app.listen(PORT, () => {
-  figlet(
-    process.env.NAME_APP,
-    {
-      font: "Big Money-ne",
-      horizontalLayout: "full",
-      verticalLayout: "default",
-      width: 180,
-      whitespaceBreak: false,
-    },
-    function (err, data) {
-      if (err) {
-        console.log("Something went wrong...");
-        x;
-        console.dir(err);
-        return;
-      }
-      console.log(data);
-      console.log(`Server is running on port ${PORT}.`);
-    }
-  );
+const port = process.env.PORT || 80;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
 });
+// app.listen(PORT, () => {
+//   console.log(`Our app is running on port ${PORT}`);
+//   figlet(
+//     process.env.NAME_APP,
+//     {
+//       font: "Big Money-ne",
+//       horizontalLayout: "full",
+//       verticalLayout: "default",
+//       width: 180,
+//       whitespaceBreak: false,
+//     },
+//     function (err, data) {
+//       if (err) {
+//         console.log("Something went wrong...");
+//         x;
+//         console.dir(err);
+//         return;
+//       }
+//       console.log(data);
+//       console.log(`Server is running on port ${PORT}.`);
+//     }
+//   );
+// });
 
 module.exports = app;
