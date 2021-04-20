a0a4ca5c1b9bb4d390798befa053969d0948acde
Alexanne Brunold <alexanne.brunold@hetic.net>
2021-04-20 16:51:34 +0200

    ADD Terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "email_address" {
+
+  description = "Email address to use for Heroku"
+
+  type        = string
+
+  sensitive   = true
+
+}
+
+variable "heroku_api_key" {
+
+  description = "Your Heroku API key"
+
+  type        = string
+
+  sensitive   = true
+
+}
