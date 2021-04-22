02760c9f06a1eb2af7cc72c49e299af8c59e75b5
Lucas Moreno <moreno.lucas.82300@hotmail.fr>
2021-04-22 15:06:43 +0200

    modif method mocha

--- test/test.0-auth.js
+++ test/test.0-auth.js
@@ -1,11 +1,12 @@
 const supertest = require("supertest")
-const chai = require("chai")
 const app = require("../index.js")
-const { expect } = require("chai")
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+chai.use(chaiHttp);
 
 
 global.app = app
-global.expect = chai.expect
 global.request = supertest(app)
 
 /* Variables for test */
@@ -14,20 +15,52 @@ global.token
 global.testToken
 
 
-describe("Test task API Routes Auth", function () {
 
-  it("POST /auth/user/login : Log user", function (done) {
-    const newLogin = {
-      "email": "salsa@gmail.com",
-      "password": "salsa69"
-    }
-    request
-      .post("/auth/user/login")
-      .send(newLogin)
-      .end(function (err, res) {
-        global.token = res.body.data
-        global.testToken = global.token
-        done(err)
-      })
-  })
-})
+
+
+
+
+// describe("Test task API Routes Auth", function () {
+
+//   it("POST /auth/user/signIn : Log user", function (done) {
+//     const newLogin = {
+//       "email": "lucasfront@gmail.com",
+//       "password": "lucasfront"
+//     }
+//     request
+//       .post("/auth/user/login")
+//       .send(newLogin)
+//       .end(function (err, res) {
+//         global.token = res.body.data
+//         global.testToken = global.token
+//         expect(res.status).to.equal(201)
+//         done(err)
+//       })
+//   })
+// })
+
+describe('/POST user', () => {
+
+    it('POST /auth/user/login : Log user', (done) => {
+        const newLogin = {
+        "email": "salsa@gmail.com",
+        "password": "salsa69"
+        }
+      chai.request(app)
+
+          .post('/auth/user/login')
+          .send(newLogin)
+          .end((err, res) => {
+                res.should.have.status(201);
+
+                // res.body.should.be.a('array');
+
+                // res.body.length.should.be.eql(0);
+
+            done();
+
+          });
+
+    });
+
+});
